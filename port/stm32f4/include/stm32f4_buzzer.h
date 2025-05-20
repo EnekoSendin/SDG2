79ccb4c473fe5b8333d6d4f8576185a34a9018e2
EnekoSendin <eneko.sendin@gmail.com>
2025-05-20 17:44:52 +0200

    Update stm32f4_buzzer.h, comentario

--- port/stm32f4/include/stm32f4_buzzer.h
+++ port/stm32f4/include/stm32f4_buzzer.h
@@ -1,9 +1,9 @@
 /**
  * @file stm32f4_buzzer.h
  * @brief Header for stm32f4_buzzer.c file.
- * @author alumno1
- * @author alumno2
- * @date fecha
+ * @author Eneko Emilio Sendín Gallastegi
+ * @author Rodrigo Gutiérrez Fontán 
+ * @date 2025-05-20
  */
 #ifndef STM32F4_BUZZER_SYSTEM_H_
 #define STM32F4_BUZZER_SYSTEM_H_
@@ -18,7 +18,7 @@
 /* Defines */
 
 //TIM8 channel2 PC7
-#define STM32F4_PARKING_BUZZER_GPIO GPIOC
-#define STM32F4_PARKING_BUZZER_PIN 7
+#define STM32F4_PARKING_BUZZER_GPIO GPIOC /*!< PUERTO del buzzer */
+#define STM32F4_PARKING_BUZZER_PIN 7 /*!< PIN del buzzer */
 
 #endif /* STM32F4_BUZZER_SYSTEM_H_ */
