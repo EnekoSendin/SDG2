f23612d5427794b20155bebede5ed92762cf7ece
EnekoSendin <eneko.sendin@gmail.com>
2025-05-20 17:52:17 +0200

    Update stm32f4_display.h, te comento

--- port/stm32f4/include/stm32f4_display.h
+++ port/stm32f4/include/stm32f4_display.h
@@ -1,9 +1,9 @@
 /**
  * @file stm32f4_display.h
  * @brief Header for stm32f4_display.c file.
- * @author alumno1
- * @author alumno2
- * @date fecha
+ * @author Eneko Emilio Sendín Gallastegi
+ * @author Rodrigo Gutiérrez Fontán
+ * @date 2025-05-20
  */
 #ifndef STM32F4_DISPLAY_SYSTEM_H_
 #define STM32F4_DISPLAY_SYSTEM_H_
@@ -16,16 +16,16 @@
 
 /* Defines and enums ----------------------------------------------------------*/
 /* Defines */
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_R_GPIO GPIOB
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_R_GPIO GPIOB /*!< PUERTO del led rojo*/
  
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_R_PIN 6
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_R_PIN 6 /*!< PIN del led rojo*/
  
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_G_GPIO GPIOB
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_G_GPIO GPIOB /*!< PUERTO del led verde*/
  
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_G_PIN 8
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_G_PIN 8 /*!< PIN del led verde*/
  
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_B_GPIO GPIOB
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_B_GPIO GPIOB /*!< PUERTO del led azul*/
  
-#define STM32F4_REAR_PARKING_DISPLAY_RGB_B_PIN 9
+#define STM32F4_REAR_PARKING_DISPLAY_RGB_B_PIN 9 /*!< PIN del led azul*/
 
 #endif /* STM32F4_DISPLAY_SYSTEM_H_ */
